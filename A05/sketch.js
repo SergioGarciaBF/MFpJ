25337b08c508c230c0a6390fe1df5e96878cae5f
Sérgio Garcia <88694066+SergioGarciaBF@users.noreply.github.com>
2023-12-07 22:29:43 -0300

    Adição das linhas/barreiras

--- A05/sketch.js
+++ A05/sketch.js
@@ -1,4 +1,6 @@
+//==========================================================================================
 //Funções básicas de manipulação vetorial - Estão melhoradas em relação ao trabalho anterior
+//==========================================================================================
 
 class Vec3 {
     constructor (x, y, z) {
@@ -13,7 +15,7 @@ class Vec3 {
     console.log(`x: ${v.x},  y: ${v.y}, z: ${v.z}`);
   }
   
-  //Comprimento do vetor:
+  //Comprimento do vetor - Magnitude de um vetor:
   function length (v) {
     return Math.sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
   }
@@ -21,6 +23,10 @@ class Vec3 {
   //Multiplicação por escalar:
   function mult(v, s) {
     return new Vec3(v.x*s, v.y*s, v.z*s)
+  }
+  
+  function div(v, s) {
+    return new Vec3(v.x/s, v.y/s, v.z/s)
   }
   
   //Soma entre dois vetores:
@@ -48,8 +54,13 @@ class Vec3 {
   
   //Normalizar um vetor:
   function normalize (v) {
-      
+      return div(v, length(v))
   }
+  
+  
+  //==========================================================================================
+  //Física: Partículas e Barreiras
+  //==========================================================================================
   
   //Definição de uma partícula:
   class Particula {
@@ -72,9 +83,17 @@ class Vec3 {
       fill(0)
       ellipse(this.position.x, this.position.y, 10, 10)
     }
+    
+    ricochetear () {
+      let alpha = 1
+      let beta = 1
+      let vn = dot(n,v)
+      let vp = v-vn
+      v = sub(mult(vp,alpha), mult(vp, beta))
+    }
   }
   
-  //Definição de uma barreira:
+  //Definição de uma barreira - linha:
   class Barreira {
     
     constructor (xInicial, yInicial, xFinal, yFinal){
@@ -86,6 +105,54 @@ class Vec3 {
       stroke(color(0))
       strokeWeight(5)
       line(this.inicio.x, this.inicio.y, this.fim.x, this.fim.y)
+    }
+    
+    cruzamento (linha) {
+      let A = this.inicio
+      let B = this.fim
+      let C = linha.inicio
+      let D = linha.fim
+    
+      let AB = sub(B,A);
+      let AC = sub(C,A);
+      let AD = sub(D,A);
+    
+      let AB_x_AC = AB.cross(AC);
+      let AB_x_AD = AB.cross(AD);
+    
+      let CD = sub(D,C);
+      let CA = sub(A,C);
+      let CB = sub(B,C);
+    
+      let CD_x_CA = CD.cross(CA);
+      let CD_x_CB = CD.cross(CB);
+    
+      //Verifica se não houve colisão
+      if ((AB_x_AC.z * AB_x_AD.z) > 0) {
+        return false;
+        
+  
+      }else if ((CD_x_CA.z * CD_x_CB.z) > 0) {
+        return false;
+      //Em caso de colisão, é necessa'rio verificar se as retas são colineares
+      } else if(length(pv(AB, CD))==0) { //Magnitude do produto vetorial
+        return false
+    
+      }
+  
+      return true
+    }
+    
+    //Método para indicar a direção de um vetor
+    dir(){
+      return sub(this.fim,this.inicio)
+    }
+    
+    //Método para gerar a normal de uma linha
+    normal () {
+      let n = this.dir()
+      let v = new Vec3(-n.y, n.x)
+      return normalize(v) //Retorna o vetor normalizado
     }
     
     
@@ -100,7 +167,8 @@ class Vec3 {
   let listaParticulas = []
   
   function setup() {
-    createCanvas(800, 800);
+    createCanvas(600, 600);
+    let cima = barreira()
     particula = new Particula (5,5)
   }
   
@@ -111,3 +179,5 @@ class Vec3 {
     particula.mostrar();
     particula.movimentar();
   }
+  
+  
